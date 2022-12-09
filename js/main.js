867bdb3b8f30d6dcc4cb1759dd0d941f6c9c6d3c
MinjiKim-1 <pcodelab@gmail.com>
2022-12-09 14:29:55 +0900

    - CSS 디자인 적용 완료, slideMain pagination 활성화 추가, btnMore mixin 적용, 모션 적용 (visual 라인박스, introduce 탭, magazine 슬라이드, skill&concpet 라인효과, collection 호버효과)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
-/* 변수 선언 */
-const sliderMain = document.querySelector("figure #sliderMain");
+/* sliderMain - 변수선언 */
+const sliderMain = document.querySelector("#visual #sliderMain");
 const ul = sliderMain.querySelector("ul");
 const btnPrev = sliderMain.querySelector(".btnPrev");
 const btnNext = sliderMain.querySelector(".btnNext");
@@ -11,9 +11,44 @@ let len = lis.length;
 let speed = 500;
 let enableClick = true;
 
-//초기화 함수 호출
-init();
+/* lineBox - 변수선언 */
+const lineBox = document.querySelector("#visual .lineBox")
+const _top = lineBox.querySelector(".top");
+const _right = lineBox.querySelector(".right");
+const _bottom = lineBox.querySelector(".bottom");
+const _left = lineBox.querySelector(".left");
+const _content = lineBox.querySelector(".content")
 
+/* subTab - 변수선언 */
+const subTab = document.querySelectorAll("#introduce #subTab li");
+const subPage = document.querySelectorAll("#introduce article");
+
+/* slideSub - 변수선언 */
+const sliderSub = document.querySelector("#magazine #sliderSub");
+const btnBox = sliderSub.querySelector(".btnBox");
+const imgSub = sliderSub.querySelectorAll(".pic img");
+const imgLen = imgSub.length - 1;
+let i = 0;
+
+
+
+
+/* subTab */
+subTab.forEach((el, index) => {
+    el.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        let isOn = e.currentTarget.classList.contains("on");
+        if (isOn) return;
+
+        activation(subTab, index);
+        activation(subPage, index);
+    })
+})
+
+
+/* sliderMain */
+init();
 
 btnNext.addEventListener("click", (e) => {
     e.preventDefault();
@@ -28,7 +63,7 @@ btnNext.addEventListener("click", (e) => {
 
 
 btnPrev.addEventListener("click", (e) => {
-    e.preventDefault();
+    e.preventDefault(e);
 
     let isOn = e.currentTarget.classList.contains("on");
     if (isOn) return;
@@ -38,6 +73,82 @@ btnPrev.addEventListener("click", (e) => {
     }
 })
 
+paging.forEach((el, index) => {
+    el.addEventListener("click", () => {
+        for (let el of paging) el.classList.remove("on");
+        new Anim(ul, {
+            prop: "left",
+            value: "-200%",
+            duration: speed,
+            callback: () => {
+                ul.append(ul.firstElementChild)
+                ul.style.left = "-100%";
+                enableClick = true;
+            }
+        })
+        el.classList.add("on");
+    })
+})
+
+
+
+/* lineBox */
+window.onload = function () {
+    new Anim(_content, {
+        prop: "opacity",
+        value: 1,
+        duration: speed,
+    })
+    new Anim(_top, {
+        prop: "width",
+        value: "100%",
+        duration: speed,
+        callback: () => {
+            new Anim(_right, {
+                prop: "height",
+                value: "100%",
+                duration: speed,
+                callback: () => {
+                    new Anim(_bottom, {
+                        prop: "width",
+                        value: "100%",
+                        duration: speed,
+                        callback: () => {
+                            new Anim(_left, {
+                                prop: "height",
+                                value: "100%",
+                                duration: speed,
+                                // callback: () => {
+                                //     new Anim(_content, {
+                                //         prop: "opacity",
+                                //         value: 1,
+                                //         duration: speed,
+                                //     })
+                                // }
+                            })
+                        }
+                    })
+                }
+            })
+        }
+    })
+}
+
+
+/* slideSub */
+btnBox.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (i == imgLen) {
+        i = 0;
+    } else {
+        i++;
+    }
+    activation(imgSub, i);
+})
+
+
+/* 함수 패키징 */
 
 function init() {
     ul.style.left = "-100%";
@@ -90,3 +201,6 @@ function pagination(n) {
     let pageNow = pageArr[n].getAttribute("data-index");
     paging[pageNow].classList.add("on");
 }
+
+
+
