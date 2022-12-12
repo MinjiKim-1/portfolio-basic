977d89d1d7c2e3697a7b357ec86e96c808c00f87
MinjiKim-1 <pcodelab@gmail.com>
2022-12-13 08:53:06 +0900

    모바일메뉴 리셋 적용, about 슬라이드방식 변경

--- js/about.js
+++ js/about.js
@@ -1,59 +1,32 @@
 /* sliderText - 변수선언 */
 const sliderText = document.querySelector("#role #sliderText");
 const innerBox = sliderText.querySelector(".inner");
-const btnBox = sliderText.querySelector(".btnBox");
-
-const articles = innerBox.querySelectorAll("article");
-let len = articles.length;
-let speed = 500;
-let enableClick = true;
+const btns = sliderText.querySelectorAll(".btns li");
 
 
-init();
+btns.forEach((el, index) => {
+    el.addEventListener("click", e => {
+        e.preventDefault();
 
-btnBox.addEventListener("click", (e) => {
-    e.preventDefault();
+        let isOn = e.currentTarget.classList.contains("on");
+        if (isOn) return;
 
-    let isOn = e.currentTarget.classList.contains("on");
-    if (isOn) return;
-    if (enableClick) {
-        nextSlide(2);
-        enableClick = false;
-    }
-
-    for (let i = 0; i > len; i++) {
-        activation(articles, i);
-    }
-
+        activation(index);
+    })
 })
 
 
-
-/* 함수 패키징 */
-
-function init() {
-    innerBox.style.left = "-100%";
-    innerBox.style.width = `${100 * len}%`;
-    articles.forEach((el) => {
-        el.style.width = `${100 / len}%`;
-    })
-    innerBox.prepend(innerBox.lastElementChild)
-}
-
-function nextSlide(n) {
+function activation(index) {
     new Anim(innerBox, {
-        prop: "left",
-        value: -100 * n + "%",
-        duration: speed,
-        callback: () => {
-            innerBox.append(innerBox.firstElementChild)
-            innerBox.style.left = "-100%";
-            enableClick = true;
-        }
-    })
-}
+        prop: "margin-left",
+        value: -100 * index + "%",
+        duration: 1000,
+    });
 
-function activation(arr, index) {
-    for (let el of arr) el.classList.remove("on");
-    arr[index].classList.add("on");
-}
+    // 버튼 활성화
+    for (let btn of btns) {
+        btn.classList.remove("on");
+    };
+    btns[index].classList.add("on");
+};
+
