4ea4fedb9656887cfa70d454b72736f61bd5e331
MinjiKim-1 <pcodelab@gmail.com>
2022-12-19 08:56:23 +0900

    About 하단 영상으로 교체, Serves 삭제, 카카오맵 마커오류 해결, Join페이지 CSS 작업

--- js/contact.js
+++ js/contact.js
@@ -7,32 +7,36 @@ const branch_btns = document.querySelectorAll("#location .branch li");
 
 
 mapOption = {
-    center: new kakao.maps.LatLng(37.5258975, 126.9284261),
+    center: new kakao.maps.LatLng(37.5632348, 126.9846716),
     level: 3
 };
 
+/* 지도 생성 */
 var map = new kakao.maps.Map(mapContainer, mapOption);
+
+
+/* 마커 생성 */
 var markerOptions = [
     {
         title: "Main Store",
         latLng: new kakao.maps.LatLng(37.5632348, 126.9846716),
         imgSrc: "img/marker1.png",
-        imgSize: new kakao.maps.Size(232, 99),
-        imgPos: { offset: new kakao.maps.Point(116, 69) },
+        imgSize: new kakao.maps.Size(152, 85),
+        imgPos: { offset: new kakao.maps.Point(76, 55) },
         button: branch_btns[0],
     }, {
         title: "Branch 1",
         latLng: new kakao.maps.LatLng(36.3525117, 127.3779744),
         imgSrc: "img/marker2.png",
-        imgSize: new kakao.maps.Size(232, 99),
-        imgPos: { offset: new kakao.maps.Point(116, 69) },
+        imgSize: new kakao.maps.Size(152, 85),
+        imgPos: { offset: new kakao.maps.Point(76, 55) },
         button: branch_btns[1],
     }, {
         title: "Branch 2",
         latLng: new kakao.maps.LatLng(35.1529425, 129.0595638),
         imgSrc: "img/marker3.png",
-        imgSize: new kakao.maps.Size(232, 99),
-        imgPos: { offset: new kakao.maps.Point(116, 69) },
+        imgSize: new kakao.maps.Size(152, 85),
+        imgPos: { offset: new kakao.maps.Point(76, 55) },
         button: branch_btns[2],
     }
 ];
@@ -40,7 +44,6 @@ var markerOptions = [
 
 for (let i = 0; i < markerOptions.length; i++) {
 
-    /* 마커 생성 함수 */
     new kakao.maps.Marker({
         map: map,
         position: markerOptions[i].latLng,
@@ -63,6 +66,8 @@ for (let i = 0; i < markerOptions.length; i++) {
 }
 
 
+
+
 /* 화면이 resize 될 경우 지도 위치를 자동 재정렬하는 함수 */
 window.onresize = () => {
 
@@ -73,29 +78,47 @@ window.onresize = () => {
 }
 
 
+/* 교통정보 표시 */
+// t_on.addEventListener("click", (e) => {
+//     e.preventDefault();
+
+//     if (t_on.classList.contains("on")) return;
+
+//     map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+
+//     t_on.classList.add("on");
+//     t_off.classList.remove("on");
+
+// });
+
+// t_off.addEventListener("click", (e) => {
+//     e.preventDefault();
+
+//     if (t_off.classList.contains("on")) return;
+
+//     map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+
+//     t_off.classList.add("on");
+//     t_on.classList.remove("on");
+
+// });
+
+
+
 t_on.addEventListener("click", (e) => {
     e.preventDefault();
-
-    if (t_on.classList.contains("on")) return;
-
-    map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-
-    t_on.classList.add("on");
-    t_off.classList.remove("on");
-
+    if (!t_on.classList.contains("on")) {
+        t_on.classList.toggle("on");
+        map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+    } else {
+        t_on.classList.toggle("on");
+        map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+    }
 });
 
-t_off.addEventListener("click", (e) => {
-    e.preventDefault();
 
-    if (t_off.classList.contains("on")) return;
 
-    map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
 
-    t_off.classList.add("on");
-    t_on.classList.remove("on");
-
-});
 
 
 /* 지도타입 전환 */
